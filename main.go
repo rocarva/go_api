2536c7ea5caa7984aade096d34d5f0fae5737f4d
Rodrigo Carvalho <ro051186@hotmail.com>
2023-01-19 18:42:29 -0300

    projeto final

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"personalidades/database"
+	"personalidades/models"
+	"personalidades/routes"
+)
+
+func main() {
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
+	}
+	database.ConectaComBancoDeDados()
+	routes.HandleRequest()
+}
